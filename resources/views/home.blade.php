760be069d153e2cb17293bec7a409105b5277ace
Buendia <kenneth.buendia.code@gmail.com>
2024-05-04 01:07:45 +0000

    create home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!--2024/05/04/09:56 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gusto</title>
+</head>
+<body>
+    <h1>GUSTO</h1>
+</body>
+</html>
